32574052da500b959caa5830335d4bbc683ee003
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-04-16 11:47:01 +0200

    updates

---
+++ src/cut_video.py
@@ -0,0 +1,44 @@
+from os import remove
+from os.path import isfile
+
+# Use moviepy since it is faster than opencv
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip
+
+import proglog
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+def cut(input_video: str, output_video: str, t1: int = None, t2: int = None, clear: bool = True, log: bool = True) -> str:
+
+    if log:
+        logging.info(f"Processing: {input_video}")
+
+    # Disable moviepy command logs
+    #! This line invalidate the default_bar_logger for the entire script!
+    proglog.default_bar_logger = lambda *args, **kwargs: lambda *inner_args, **inner_kwargs: None
+
+    # Verify that video exists
+    assert isfile(input_video), f"Video '{input_video}' doesn't exist"
+
+    # Delete output video if it alrady exists and if clear flag is enabled
+    if clear and isfile(output_video):
+        remove(output_video)
+
+    # Open video
+    clip = VideoFileClip(filename=input_video) 
+
+    # Close it, otherwise we can have handling problems
+    clip.reader.close()
+
+    # Extract subclip
+    t1 = t1 if t1 is not None else 0
+    t2 = t2 if t2 is not None else clip.duration
+
+    ffmpeg_extract_subclip(filename=input_video, t1=t1, t2=t2, targetname=output_video)
+
+    if log:
+        logging.info(f"Done: {output_video}")
+
+    return output_video
---
+++ src/utils.py
@@ -0,0 +1,52 @@
+import cv2
+import screeninfo
+from uuid import uuid4
+from typing import Union
+
+def show_img(mat: Union[cv2.typing.MatLike, cv2.cuda.GpuMat, cv2.UMat, list[cv2.typing.MatLike], list[cv2.cuda.GpuMat], list[cv2.UMat]], winname: Union[str, list[str]] = "", ratio: float = 2) -> None:
+
+    # Get monitor info in order to calculate the best size for the image(s)
+    monitor = screeninfo.get_monitors()[0]
+
+    def _process_image(m):
+        nonlocal monitor, ratio
+
+        # Copy the image
+        _mat = m.copy()
+
+        # Calculate new height
+        height = int(monitor.height / ratio)
+
+        # Calculate new height proportional to the width
+        width = int(height * _mat.shape[1] / _mat.shape[0])
+        winsize = (width, height)
+
+        # Resize the image
+        _mat = cv2.resize(_mat, winsize)
+
+        return _mat
+
+    # Prepare image(s) and label(s)
+    if not isinstance(mat, list):
+        assert isinstance(winname, str), "winname must be a string"
+
+        mat = [mat]
+        winname = [winname]
+    
+    else:
+        if not isinstance(winname, list):
+            winname = [str(uuid4()) for _ in range(len(mat))]
+        else:
+            assert len(winname) == len(mat), "winname must have the same length of mat"
+            assert len(winname) == len(set(winname)), "winnames must be unique"
+
+    for i in range(len(mat)):
+
+        # Process the image
+        tmp = _process_image(m=mat[i])
+
+        # Show the image
+        cv2.imshow(winname=winname[i], mat=tmp)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
