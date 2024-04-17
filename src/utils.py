09c788d4a95b4e79f0b572a439aaa293799b6033
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-04-17 10:15:50 +0200

    updates

--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,39 @@
 import cv2
+import numpy as np
 import screeninfo
 from uuid import uuid4
 from typing import Union
 
-def show_img(mat: Union[cv2.typing.MatLike, cv2.cuda.GpuMat, cv2.UMat, list[cv2.typing.MatLike], list[cv2.cuda.GpuMat], list[cv2.UMat]], winname: Union[str, list[str]] = "", ratio: float = 2) -> None:
+def auto_resize(mat: Union[cv2.typing.MatLike, cv2.cuda.GpuMat, cv2.UMat], ratio: float = 2) -> np.ndarray:
 
     # Get monitor info in order to calculate the best size for the image(s)
     monitor = screeninfo.get_monitors()[0]
 
-    def _process_image(m):
-        nonlocal monitor, ratio
+    # Copy the image
+    _mat = mat.copy()
 
-        # Copy the image
-        _mat = m.copy()
+    if monitor.height < monitor.width:
+        # Calculate new height
+        height = int(monitor.height / ratio)
 
-        if monitor.height < monitor.width:
-            # Calculate new height
-            height = int(monitor.height / ratio)
+        # Calculate new width proportional to the height
+        width = int(height * _mat.shape[1] / _mat.shape[0])
+    
+    else:
+        # Calculate new width
+        width = int(monitor.width / ratio)
 
-            # Calculate new width proportional to the height
-            width = int(height * _mat.shape[1] / _mat.shape[0])
-        
-        else:
-            # Calculate new width
-            width = int(monitor.width / ratio)
+        # Calculate new height proportional to the width
+        height = int(width * _mat.shape[0] / _mat.shape[1])
 
-            # Calculate new height proportional to the width
-            height = int(width * _mat.shape[0] / _mat.shape[1])
+    winsize = (width, height)
 
-        winsize = (width, height)
+    # Resize the image
+    _mat = cv2.resize(_mat, winsize)
 
-        # Resize the image
-        _mat = cv2.resize(_mat, winsize)
+    return _mat
 
-        return _mat
+def show_img(mat: Union[cv2.typing.MatLike, cv2.cuda.GpuMat, cv2.UMat, list[cv2.typing.MatLike], list[cv2.cuda.GpuMat], list[cv2.UMat]], winname: Union[str, list[str]] = "", ratio: float = 2) -> None:
 
     # Prepare image(s) and label(s)
     if not isinstance(mat, list):
@@ -52,7 +52,7 @@ def show_img(mat: Union[cv2.typing.MatLike, cv2.cuda.GpuMat, cv2.UMat, list[cv2.
     for i in range(len(mat)):
 
         # Process the image
-        tmp = _process_image(m=mat[i])
+        tmp = auto_resize(mat=mat[i], ratio=ratio)
 
         # Show the image
         cv2.imshow(winname=winname[i], mat=tmp)
