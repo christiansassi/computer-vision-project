4fb8d32c3bba292b82bb14c0fdceb9479c8e0979
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-10-21 17:41:20 +0200

    updated ball tracking

--- src/ball_tracking.py
+++ src/ball_tracking.py
@@ -39,7 +39,9 @@ class ParticleFilter:
             ParticleFilter.COLORS.extend(new_colors)
 
         self.color = ParticleFilter.COLORS.pop()
-            
+        
+        self.points = []
+
     def __del__(self):
 
         # try-except block used for handling script interruptions and the removal of the class
@@ -98,151 +100,75 @@ class ParticleFilter:
 
         return estimate_position, self.particles.copy()
 
+    def add_point(self, point: tuple):
+
+        if len(self.points) == params.MAX_POINTS:
+            self.points.pop(0)
+
+        self.points.append(point)
+    
+    def get_points(self) -> list:
+        return self.points.copy()
+
     def get_distance(self, centroid: np.ndarray) -> int:
         return math.dist(centroid, self.centroid)
 
     def get_color(self):
         return (0, 0, 255) #tuple(list(self.color).copy())
 
-def particle_filtering(mat: cv2.typing.MatLike | cv2.cuda.GpuMat | cv2.UMat, bounding_boxes: list[tuple], reset: bool = False) -> dict:
+def particle_filtering(mat: cv2.typing.MatLike | cv2.cuda.GpuMat | cv2.UMat, bounding_box: list[tuple], reset: bool = False) -> dict:
 
-    frame = mat.copy()
     height, width = mat.shape[:2]
 
     function = eval(inspect.stack()[0][3])
 
     try:
-        # Check if the current function has particle systems
-        function.particle_systems
+        # Check if the current function has a particle system
+        function.particle_system
     except:
         reset = True
 
     if reset:
-        # Reset and initialize the particle systems for each bounding box
-        function.particle_systems = []
+        # Reset and initialize the particle system
+        function.particle_system = function.particle_system = ParticleFilter(width=width, height=height, number_of_particles=params.NUMBER_OF_PARTICLES, bounding_box=bounding_box)
 
-        for bounding_box in bounding_boxes:
-            function.particle_systems.append(ParticleFilter(width=width, height=height, number_of_particles=params.NUMBER_OF_PARTICLES, bounding_box=bounding_box))
-
-    # Make a copy of the particle systems
-    particle_systems = function.particle_systems.copy()
-
-    # Calculate the distance between each bounding box and every particle system
-    distances = {}
-
-    for bounding_box in bounding_boxes:
-        x, y, w, h = bounding_box
-        centroid = np.array([x + w / 2, y + h / 2])
-
-        distances[bounding_box] = []
-
-        for particle_system in particle_systems:
-            distances[bounding_box].append([particle_system, particle_system.get_distance(centroid=centroid)])
-
-    # Associate the best particle system to each bounding box based on distance
-    best_distances = {}
-
-    # Iterate through each bounding box to find the best particle system
-    for bounding_box1 in distances:
-        
-        # Copy the particle system-distance pairs for the current bounding box
-        distances_for_box1 = distances[bounding_box1].copy()
-
-        while True:
-            # Get the particle system with the smallest distance for this bounding box
-            best_particle_system1, best_distance1 = min(distances_for_box1, key=lambda item: item[1])
-
-            # Ensure the selected particle system is truly the best for this bounding box by comparing distances with other bounding boxes
-            keep = True
-
-            for bounding_box2 in distances:
-                if bounding_box1 == bounding_box2:
-                    continue
-
-                # Compare with the best particle system for the other bounding box
-                best_particle_system2, best_distance2 = min(distances[bounding_box2], key=lambda item: item[1])
-
-                # If another bounding box has a better claim to the particle system, re-evaluate the choice
-                if best_particle_system1 == best_particle_system2 and best_distance1 > best_distance2:
-                    keep = False
-                    break
-
-            # If the particle system isn't the best choice, remove it and continue searching
-            if not keep:
-                distances_for_box1.remove([best_particle_system1, best_distance1])
-
-                # If no more pairs are available, create a new particle system for this bounding box
-                if not len(distances_for_box1):
-
-                    best_particle_system1 = ParticleFilter(width=width, height=height, number_of_particles=params.NUMBER_OF_PARTICLES, bounding_box=bounding_box1)
-                    best_distance1 = 0
-
-                    function.particle_systems.append(best_particle_system1)
-                    best_distances[bounding_box1] = [best_particle_system1, best_distance1]
-
-                    break
-            
-            else:
-                # Save the current best particle system for this bounding box
-                best_distances[bounding_box1] = [best_particle_system1, best_distance1]
-                break
-
-    # Check if the distances exceed a threshold, indicating a possible incorrect association (e.g. new bounding box)
-    particle_systems = []
-
-    for bounding_box in best_distances:
-        particle_system, distance = best_distances[bounding_box]
-
-        # Reinitialize the particle system if the distance is too large
-        if distance > params.DISTANCE:
-            particle_system = ParticleFilter(width=width, height=height, number_of_particles=params.NUMBER_OF_PARTICLES, bounding_box=bounding_box)
-
-        particle_systems.append([particle_system, bounding_box])
-
-    # Update the particle systems and perform steps for each
-    function.particle_systems = []
-    
     results = {}
 
-    for particle_system, bounding_box in particle_systems:
-        x, y, w, h = bounding_box
-        centroid = np.array([x + w / 2, y + h / 2])
+    x, y, w, h = bounding_box
+    centroid = np.array([x + w / 2, y + h / 2])
 
-        # Update the particle system
-        estimate_position, particles = particle_system.step(centroid=centroid)
-        
-        # Save the particle system for future use
-        function.particle_systems.append(particle_system)
+    # Update the particle system
+    estimate_position, particles = function.particle_system.step(centroid=centroid)
 
-        # Correct the predictions
-        origin = (int(centroid[0]), int(centroid[1]))
-        estimated = (int(estimate_position[0]), int(estimate_position[1]))
-        
-        new_x = estimated[0] - origin[0]
-        new_y = estimated[1] - origin[1]
+    # Correct the predictions
+    origin = (int(centroid[0]), int(centroid[1]))
+    estimated = (int(estimate_position[0]), int(estimate_position[1]))
+    
+    new_x = estimated[0] - origin[0]
+    new_y = estimated[1] - origin[1]
 
-        if new_x > 0:
-            new_x = origin[0] - new_x
-        else:
-            new_x = origin[0] + new_x * -1
-        
-        if new_y > 0:
-            new_y = origin[1] - new_y
-        else:
-            new_y = origin[1] + new_y * -1
+    if new_x > 0:
+        new_x = origin[0] - new_x
+    else:
+        new_x = origin[0] + new_x * -1
+    
+    if new_y > 0:
+        new_y = origin[1] - new_y
+    else:
+        new_y = origin[1] + new_y * -1
 
-        estimated = (new_x, new_y)
+    estimated = (new_x, new_y)
 
-        # Save results
-        results[bounding_box] = {
-            "origin": origin,
-            "estimated": estimated
-        }
+    # Keep track of previous positions
+    function.particle_system.add_point(point=origin)
 
-        # Draw bounding box
-        #cv2.rectangle(frame, (x, y), (x + w, y + h), particle_system.get_color(), 2)
-
-        # Draw the arrow associated to the estimated position
-        #cv2.arrowedLine(frame, origin, estimated, particle_system.get_color(), 4, tipLength=0.25)
+    # Save results
+    results[bounding_box] = {
+        "origin": origin,
+        "estimated": estimated,
+        "particles": particles,
+        "points": function.particle_system.get_points(),
+        "color": function.particle_system.get_color()
+    }
 
     return results
